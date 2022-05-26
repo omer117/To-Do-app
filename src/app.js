c9565931987f751e0c9bbee3ba2dd2fddc53a532
omer117 <omer117.cyber4s.dev>
2022-05-26 15:42:35 +0300

    adding Functions

--- src/app.js
+++ src/app.js
@@ -1,54 +1,58 @@
-let taskCounter = 1;
-
+let taskCounter = 0;
 function addToList() {
-    //creating Labeles and inputs, setting atr and id's
     const mainDiv = document.getElementById('center');
     const newListItem = document.createElement('input');
     const seconderyDiv = document.createElement('div');
     let label = document.createElement('label');
-    seconderyDiv.classList.add('second');
-    label.setAttribute("for", "task" + taskCounter);
-    label.innerHTML = ('task' + taskCounter + "todo!!");
+    seconderyDiv.id = 'second';
+    seconderyDiv.classList.add('secondery');
     newListItem.id = "task" + taskCounter + " li";
     newListItem.type = "checkbox";
     taskCounter++;
-
-
-    //Creating Edit & Remove btns
+    let inputValue = document.getElementById("myInput").value;
+    let t = document.createTextNode(inputValue);
+    if (inputValue === '') {
+        alert("You must write something!");
+        return;
+    }
+    else {
+        label.appendChild(t);
+    }
+    localStorage.setItem('task' + taskCounter.toString(), inputValue.toString());
     const editBtn = document.createElement('button');
     const removeBtn = document.createElement('button');
+    editBtn.setAttribute('onclick', 'editTxt(this.id)');
     editBtn.classList.add('edit');
+    editBtn.innerText = "Edit";
+    editBtn.id = 'edit';
+    removeBtn.setAttribute('onclick', 'removeTxt(this.id)');
     removeBtn.classList.add('remove');
-    editBtn.innerText = 'Edit';
-    removeBtn.innerText = 'Remove';
-
-
-    //Append
+    removeBtn.id = 'remove';
+    removeBtn.innerText = "Remove";
     mainDiv.appendChild(seconderyDiv);
     seconderyDiv.appendChild(newListItem);
+    label.setAttribute("for", "task " + taskCounter);
     seconderyDiv.appendChild(label);
     seconderyDiv.appendChild(removeBtn);
     seconderyDiv.appendChild(editBtn);
-};
-
-
-//TODO Make a button EDIT function
-function edit() {
-
-
 }
-
-//TODO MAKE A BUTTON : DELETE function 
-var close = document.getElementsByClassName("remove");
-for (let i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-
-    }
+// Make a button EDIT function
+function editTxt() {
 }
-
-
-//TODO MAKE A CLEAR ALL BUTTON function
-function clearAll() {
-
-
+// MAKE A BUTTON : DELETE function 
+function removeTxt(id) {
+    let tasksDiv = document.getElementById(id).parentElement;
+    console.log(id);
+    console.log(tasksDiv);
+    if (tasksDiv)
+        tasksDiv.innerHTML = "";
+    taskCounter--;
+}
+function resetfile() {
+    let fDiv = document.getElementById('center');
+    if (fDiv)
+        fDiv.innerHTML = "";
+    taskCounter = 0;
+    localStorage.clear();
+    console.log(localStorage);
 }
--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,69 @@
-
-let taskCounter = 1;
+let taskCounter = 0;
 
 function addToList() {
-const mainDiv = document.getElementById('center')
-const newListItem = document.createElement('input');
-const seconderyDiv = document.createElement('div');
-let label = document.createElement('label');
-seconderyDiv.classList.add('second');
-newListItem.id = "task" + taskCounter + " li";
-newListItem.type = "checkbox";
-taskCounter++;
-const editBtn = document.createElement('button');
-const removeBtn = document.createElement('button');
+    const mainDiv = document.getElementById('center')
+    const newListItem = document.createElement('input');
+    const seconderyDiv = document.createElement('div');
+    let label = document.createElement('label');
+    seconderyDiv.id = 'second';
+    seconderyDiv.classList.add('secondery');
+    newListItem.id = "task" + taskCounter + " li";
+    newListItem.type = "checkbox";
+    taskCounter++;
 
-editBtn.classList.add('edit');
-removeBtn.classList.add('remove');
 
-mainDiv.appendChild(seconderyDiv);
-seconderyDiv.appendChild(newListItem);
-label.setAttribute("for", "task"+taskCounter);
-label.innerHTML =('task' +taskCounter+ "todo!!")
-seconderyDiv.appendChild(label);
-seconderyDiv.appendChild(removeBtn)
-seconderyDiv.appendChild(editBtn)
+    let inputValue = ( < HTMLInputElement > document.getElementById("myInput")).value;
+    let t = document.createTextNode(inputValue);
+    if (inputValue === '') {
+        alert("You must write something!");
+        return;
+    } else {
+        label.appendChild(t);
+    }
 
-//Make a button EDIT function
-//MAKE A BUTTON : DELETE function 
-//MAKE A CLEAR ALL BUTTON function
+    localStorage.setItem('task' + taskCounter.toString(), inputValue.toString());
 
-};
+
+    const editBtn = document.createElement('button');
+    const removeBtn = document.createElement('button');
+
+    editBtn.setAttribute('onclick', 'editTxt(this.id)')
+    editBtn.classList.add('edit');
+    editBtn.innerText = "Edit";
+    editBtn.id = 'edit';
+    removeBtn.setAttribute('onclick', 'removeTxt(this.id)')
+    removeBtn.classList.add('remove');
+    removeBtn.id = 'remove'
+    removeBtn.innerText = "Remove";
+
+
+    mainDiv.appendChild(seconderyDiv);
+    seconderyDiv.appendChild(newListItem);
+    label.setAttribute("for", "task " + taskCounter);
+    seconderyDiv.appendChild(label);
+    seconderyDiv.appendChild(removeBtn)
+    seconderyDiv.appendChild(editBtn)
+}
+
+// Make a button EDIT function
+function editTxt() {
+
+}
+// MAKE A BUTTON : DELETE function 
+function removeTxt(id: string) {
+    let tasksDiv = <HTMLElement>document.getElementById(id).parentElement;
+    console.log(id);
+    console.log(tasksDiv);
+    if (tasksDiv) tasksDiv.innerHTML = "";
+    taskCounter--;
+
+}
+
+function resetfile(){
+let fDiv = document.getElementById('center');
+if (fDiv) fDiv.innerHTML = "";
+taskCounter = 0;
+
+localStorage.clear();
+console.log(localStorage);
+}
